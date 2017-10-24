a14eba1edc6b32fd7cb36b25dd2e22478e6b60bf
林丰 <linhui@linfengdeMacBook-Air.local>
2017-10-24 14:58:54 +0800

    项目结构搭建.

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<style>
+  
+</style>
+<template>
+  <div class="container">
+    <wc-side-menu>
+    </wc-side-menu>
+  </div>
+</template>
+<script>
+  import wcSideMenu from './wc-side-menu/wcSideMenu'
+  export default {
+    name: 'App',
+    components: {
+      wcSideMenu
+    }
+  }
+</script>
---
+++ src/wc-side-menu/wcSideMenu.vue
@@ -0,0 +1,15 @@
+<style lang="less" scoped>
+.wc-side-menu-container {
+
+}
+</style>
+<template>
+	<div class="wc-side-menu-container">
+		你好
+	</div>
+</template>
+<script>
+	export default {
+		name: 'wcSideMenu'
+	}
+</script>
