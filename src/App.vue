7e904058f8481c2318072bc0e54f10b8f7b3b9a1
林丰 <linhui@linfengdeMacBook-Air.local>
2017-10-26 00:18:22 +0800

    还在实现的路上.

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,18 @@
   position: fixed;
   top:0;
   left: 0;
+  z-index: 10;
 }
 </style>
 <template>
   <div class="container">
-      <div class="top-bar" @click="showMe"></div>
-      <h1 v-for="i in 100">{{i}} Hello World</h1>
+      <div class="top-bar" @click="showMe">1223</div>
+      <div class="content">
+        <h1 v-for="i in 100">{{i}} Hello World</h1>
+      </div>
+      
 
-      <wc-side-menu :show="show">
+      <wc-side-menu :show="show" content=".content" bar=".top-bar">
         老子是侧边栏
       </wc-side-menu>
   </div>
@@ -30,7 +34,7 @@
     },
     data () {
       return {
-        show: true
+        show: false
       }
     },
     methods: {
--- src/wc-side-menu/wcSideMenu.vue
+++ src/wc-side-menu/wcSideMenu.vue
@@ -7,6 +7,7 @@
 	background: yellow;
 	transition-duration: 200ms;
 	transform: translate(-100%);
+	z-index: 100;
 }
 .show {
 	transform: translate(0);
@@ -14,27 +15,96 @@
 </style>
 <template>
 
-	<div class="wc-side-menu-container" :class="{show: show}">
+	<div class="wc-side-menu-container" :class="{show: show}" ref="drawer">
 		<slot/>
 	</div>
 </template>
 <script>
+	/* 如果放在 handler 中, 每次都相当于是新建, 导致 add 和 remove 中的 handler 实际上不一样*/
+	let handler =  e => e.preventDefault();
 	export default {
 		name: 'wcSideMenu',
 		props: {
 			show: {
 				default: false
+			},
+			content: {
+				default: ''
+			},
+			bar: {
+				default: ''
 			}
 		},
 		watch: {
 			show (n) {
-				console.log(n)
-				let handler =  e => e.preventDefault();
+				// console.log(n)
+				// console.log(this.content)
+
+				// let content = document.querySelector(this.content);
+				// console.log(content)
+
+				
+
 				if (n) {
 					document.addEventListener('touchmove', handler, false);
+
+					this.slide();
+
 				} else {
+					// console.log('revmoe', handler)
+					// console.log(document.removeEventListener)
 					document.removeEventListener('touchmove', handler, false);
+					this.back();
+					// console.log('dddddd')
 				}
+			}
+		},
+		mounted () {
+
+		},
+		methods: {
+			slide () {
+				let content = document.querySelector(this.content);
+				let bar = document.querySelector(this.bar)
+
+				console.log(this.$refs.drawer.clientWidth);
+				let w = this.$refs.drawer.clientWidth
+
+				content.style.transitionDuration = '200ms';
+				content.style.transform = 'translateX(' + w + 'px)'
+
+				bar.style.transitionDuration = '200ms';
+				bar.style.transform = 'translateX(' + w + 'px)'
+
+
+				let handler = e => {
+					
+				}
+
+				content.addEventListener('touchstart', handler, false);
+				content.addEventListener('touchmove', handler, false);
+				content.addEventListener('touchend', handler, false);
+
+
+
+
+
+
+			},
+			back () {
+				let content = document.querySelector(this.content);
+				let bar = document.querySelector(this.bar)
+
+				console.log(this.$refs.drawer.clientWidth);
+				let w = this.$refs.drawer.clientWidth
+
+				content.style.transitionDuration = '200ms';
+				content.style.transform = 'translateX(' + 0 + 'px)'
+
+				bar.style.transitionDuration = '200ms';
+				bar.style.transform = 'translateX(' + 0 + 'px)'
+
+
 			}
 		}
 	}
